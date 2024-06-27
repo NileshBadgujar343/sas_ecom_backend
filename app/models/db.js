20e5e587f60f4721acb31920a318172184441c24
NileshBadgujar343 <nileshbadgujar36@gmail.com>
2024-06-27 12:01:52 +0530

    deploy vercel update

--- app/models/db.js
+++ app/models/db.js
@@ -9,10 +9,10 @@ const dbConfig = require("../config/dbconfig.js")
 //   database: dbConfig_DB
 // });
 const connection = mysql2.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "MySQL3307",
-  database: "ecom",
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
 });
 
 
