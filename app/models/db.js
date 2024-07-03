6a9d5c5a2d52b1b3db3243d35d9886bd6e69a461
NileshBadgujar343 <139134177+NileshBadgujar343@users.noreply.github.com>
2024-07-03 18:11:50 +0530

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,53 +1,30 @@
 // 
 
 
-const mysql = require('mysql2');
+const mysql2 = require('mysql2');
 const dbConfig = require('../config/dbconfig.js');
 
 
 
-// const connection = mysql2.createConnection({
-//   host: dbConfig.HOST,
-//   user: dbConfig.USER,
-//   password: dbConfig.PASSWORD,
-//   database: dbConfig.DB,
-//   // keepAliveInitialDelay: 10000, 
-//   // enableKeepAlive: true, 
-//   connectionLimit: 10,
-//   waitForConnections: true,
-//   queueLimit: 0
-// });
-function connection(){
-  try{
-  const pool = mysql.createPool({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
-    connectionLimit: 10,
-    waitForConnections: true,
-    queueLimit: 0
-    
-  });
-  const promisePool = pool.promise();
-
-  return promisePool;
-}catch (error) {
-  return console.log(`Could not connect - ${error}`);
-}
-  
-}
+const connection = mysql2.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB,
+  // keepAliveInitialDelay: 10000, 
+  // enableKeepAlive: true, 
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0
+});
 
 
-// connection.connect(error => {
-//   if (error) throw error;
-//   console.log("Successfully connected to the database1.");
-// });
-const pool = connection();
 
-module.exports = {
-  connection: async () => pool.getConnection(),
-  execute: (...params) => pool.execute(...params)
-};
 
-// module.exports = { connection };
+connection.connect(error => {
+  if (error) throw error;
+  console.log("Successfully connected to the database1.");
+});
+
+
+module.exports = { connection };
