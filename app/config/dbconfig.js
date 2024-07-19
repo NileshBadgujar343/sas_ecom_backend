87909a4c386348dd0ddd2637d49aada8816181a0
NileshBadgujar343 <nileshbadgujar36@gmail.com>
2024-07-19 15:26:14 +0530

    final changes

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "sql12.freesqldatabase.com",
-    USER: "sql12718832",
-    PASSWORD: "9QYKri9jXu",
-    DB: "sql12718832"
+    HOST: "srv842.hstgr.io",
+    USER: "u570148331_root",
+    PASSWORD: "Love@1435",
+    DB: "u570148331_esouqh"
   };
